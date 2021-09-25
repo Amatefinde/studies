50f56e8d8efa501cb256e0adf9161bdd53b7dbdc
iRespectOnlyYen <90966720+iRespectOnlyYen@users.noreply.github.com>
2021-09-26 01:18:29 +0300

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,70 +1,91 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
-#include "stdafx.h"
+#include <string>
+
 using namespace std;
 
 int numm1(int s)
 {
     int a, b;
+    setlocale(LC_CTYPE, "rus");
     cout << "Программа вычисляет площадь и периметр прямоугольника. ";
-    cout << "Write a and b: ";
-    cin >> a>>b;
+    cout << "Введите длины сторон a и b: ";
+    cin >> a >> b;
     cout << endl;
-    cout << "Square = " << a * b << endl;
-    cout << "Perimeter = " << 2*(a + b);
+    cout << "Площадь = " << a * b << endl;
+    cout << "Периметр = " << 2 * (a + b);
     return 0;
 }
 
 int numm2(int s) {
+    setlocale(LC_CTYPE, "rus");
     int d;
-    cout << "Write d: ";
+    cout << "Программа находит длинну оркужности. Введите длину диаметра: ";
     cin >> d;
     cout << endl;
-    cout << "Length =" << 3.14 * d;
+    cout << "Длинна окружности = " << 3.14 * d;
     return 0;
 }
 int numm3(int s) {
-    int a, b;
-    cout << "Write a and b: ";
+    double a, b;
+    setlocale(LC_CTYPE, "rus");
+    cout << "Программа находит середенее арифметическое двух чисел. Введите два числа: ";
     cin >> a >> b;
     cout << endl;
-    cout << "Average:" << (a + b) / 2;
+    cout << "Среднее арифметическое: " << (a + b) / 2;
     return 0;
 }
 int numm4(int s) {
-    int a, b;
-    cout << "Write a and b: ";
+    double a, b;
+    setlocale(LC_CTYPE, "rus");
+    cout << "Программа вычисляет сумму, разность, произведение и частное квадратов двух чисел. Введите два числа (a и b): ";
     cin >> a >> b;
     cout << endl;
-    cout << "a*a+b*b= " << a * a + b * b << endl;
-    cout << "a*a-b*b= " << a * a - b * b << endl;
-    cout << "a*a*b*b= " << a * a * b * b << endl;
-    cout << "(a*a)/(b*b)= " << (a * a) / (b * b) << endl;
+    cout << "a*a+b*b = " << a * a + b * b << endl;
+    cout << "a*a-b*b = " << a * a - b * b << endl;
+    cout << "a*a*b*b = " << a * a * b * b << endl;
+    cout << "(a*a)/(b*b) = " << (a * a) / (b * b) << endl;
     return 0;
 }
 int numm5(int s) {
-    int a, b;
-    cout << "Write a and b: ";
+    setlocale(LC_CTYPE, "rus");
+    double a, b;
+    cout << "Введите два числа a и b. Программа вычислит сумму, разность, произведение и частное их модулей. " << endl;
     cin >> a >> b;
     cout << endl;
-    cout << "|a|+|b|= " << abs(a) + abs(b) << endl;
-    cout << "|a|-|b|= " << abs(a) - abs(b) << endl;
-    cout << "|a|*|b|= " << abs(a) * abs(b) << endl;
-    cout << "|a|/|b|= " << abs(a) / abs(b) << endl;
+    cout << "|a|+|b| = " << abs(a) + abs(b) << endl;
+    cout << "|a|-|b| = " << abs(a) - abs(b) << endl;
+    cout << "|a|*|b| = " << abs(a) * abs(b) << endl;
+    cout << "|a|/|b| = " << abs(a) / abs(b) << endl;
     return 0;
 }
 
 
 int main() {
-    cout << "Select task (number 1-5): "<<endl;
+    setlocale(LC_CTYPE, "rus");
+    cout << "Введите номер задания (цифра от 1 до 5): " << endl;
     int a;
     cin >> a;
+    label1:
     if (a == 1) numm1(1);
     if (a == 2) numm2(1);
     if (a == 3) numm3(1);
     if (a == 4) numm4(1);
     if (a == 5) numm5(1);
 
-        
+    cout << endl << "Для того что-бы перейти к другой задачи введите её номер. " << endl << "Для того что бы перейти к следующей задаче введите " << "'Next'" << endl
+        << "Что бы завершить работу программы введите " << "'Exit'" << endl;
+    string name;
+    cin >> name;
+    if (name == "Exit" || name == "exit")
+        return 0;
+    if (name == "Next" || name == "next")
+        a += 1;
+    else {
+        int b = stoi(name);
+        a = b;
+    }
+    cout << "№" << a << " ";
+    goto label1;
     return 0;
 }
