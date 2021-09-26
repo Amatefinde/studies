d032b971b33f0ed274d68340c04148f899cab3f7
iRespectOnlyYen <90966720+iRespectOnlyYen@users.noreply.github.com>
2021-09-26 13:06:14 +0300

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int numm1(int s)
 {
-    int a, b;
+    double a, b;
     setlocale(LC_CTYPE, "rus");
     cout << "Программа вычисляет площадь и периметр прямоугольника. ";
     cout << "Введите длины сторон a и b: ";
@@ -19,7 +19,7 @@ int numm1(int s)
 
 int numm2(int s) {
     setlocale(LC_CTYPE, "rus");
-    int d;
+    double d;
     cout << "Программа находит длинну оркужности. Введите длину диаметра: ";
     cin >> d;
     cout << endl;
@@ -43,8 +43,8 @@ int numm4(int s) {
     cout << endl;
     cout << "a*a+b*b = " << a * a + b * b << endl;
     cout << "a*a-b*b = " << a * a - b * b << endl;
-    cout << "a*a*b*b = " << a * a * b * b << endl;
-    cout << "(a*a)/(b*b) = " << (a * a) / (b * b) << endl;
+    cout << "(a^2)*(b^2) = " << a * a * b * b << endl;
+    cout << "(a^2)/(b^2) = " << (a * a) / (b * b) << endl;
     return 0;
 }
 int numm5(int s) {
@@ -60,32 +60,42 @@ int numm5(int s) {
     return 0;
 }
 
-
 int main() {
     setlocale(LC_CTYPE, "rus");
+label2:
     cout << "Введите номер задания (цифра от 1 до 5): " << endl;
     int a;
     cin >> a;
-    label1:
+label1:
+    if (a < 1 || a > 5) {
+        cout << "Задания с таким номером не существует.\nПопробуйте ещё раз. \n\n";
+        goto label2;
+    }
+    cout << "\n№" << a << " ";
     if (a == 1) numm1(1);
     if (a == 2) numm2(1);
     if (a == 3) numm3(1);
     if (a == 4) numm4(1);
     if (a == 5) numm5(1);
 
-    cout << endl << "Для того что-бы перейти к другой задачи введите её номер. " << endl << "Для того что бы перейти к следующей задаче введите " << "'Next'" << endl
-        << "Что бы завершить работу программы введите " << "'Exit'" << endl;
+    cout << endl << "Для того что-бы перейти к другой задачи введите её номер. " << endl;
+    if (a < 5) cout << "Для того что бы перейти к следующей задаче введите " << "'Next'\n";
+    if (a > 1) cout << "Для того что бы вернуться к предыдущей задаче введите " << "'Back'\n";
+    cout << "Что бы завершить работу программы введите " << "'Exit'" << endl;
+
     string name;
     cin >> name;
-    if (name == "Exit" || name == "exit")
+    if (name == "Exit" || name == "exit" || name == "EXIT")
         return 0;
-    if (name == "Next" || name == "next")
+    if (name == "Next" || name == "next" || name == "NEXT")
         a += 1;
-    else {
-        int b = stoi(name);
-        a = b;
-    }
-    cout << "№" << a << " ";
+    else
+        if (name == "Back" || name == "back" || name == "BACK")
+            a -= 1;
+        else {
+            int b = stoi(name);
+            a = b;
+        }
     goto label1;
     return 0;
 }
